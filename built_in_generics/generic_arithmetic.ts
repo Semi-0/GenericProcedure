d77534a3eb75ca9a3758e48169741c743836e077
PterRim <longpp17@163.com>
2024-08-18 00:13:13 +0200

    added support for better set

--- built_in_generics/generic_arithmetic.ts
+++ built_in_generics/generic_arithmetic.ts
@@ -1,8 +1,10 @@
 import { construct_simple_generic_procedure, define_generic_procedure_handler } from "../GenericProcedure"
 import { all_match, match_args, one_of_args_match } from "../Predicates"
 import {  isAtom, isNull, isNumber, isString } from "./generic_predicates"
-import { isArray } from "./generic_array"
+import { isArray, filter, map, reduce } from "./generic_array"
 import { guard } from "./other_generic_helper"
+import { add_item, BetterSet, difference_set, is_better_set, merge_set, remove_item } from "./generic_better_set"
+
 
 export const add = construct_simple_generic_procedure(
     "add",
@@ -22,6 +24,17 @@ define_generic_procedure_handler(add, all_match(isArray), (a: any[], b: any[]) =
 define_generic_procedure_handler(add, match_args(isArray, isAtom), (a: any[], b: string) => {
     return [...a, b]
 }) 
+
+
+define_generic_procedure_handler(add, match_args(is_better_set, is_better_set), (a: BetterSet<any>, b: BetterSet<any>) => {
+    return merge_set(a, b)
+})
+
+define_generic_procedure_handler(add, match_args(is_better_set, isAtom), (a: BetterSet<any>, b: any) => {
+    return add_item(a, b)
+})
+
+
 
 
 export const multiply = construct_simple_generic_procedure(
@@ -60,13 +73,23 @@ export const subtract = construct_simple_generic_procedure(
 define_generic_procedure_handler(subtract,
     match_args(isArray, isAtom), 
     (a: any[], b: any) => {
-        return a.filter((x) => x !== b)
+        return filter(a, (x) => x !== b)
     }
 )
 
 define_generic_procedure_handler(subtract, match_args(isArray, isArray), (a: any[], b: any[]) => {
     return a.filter((x) => !b.includes(x))
 }) 
+
+
+define_generic_procedure_handler(subtract, match_args(is_better_set, is_better_set), (a: BetterSet<any>, b: BetterSet<any>) => {
+    return difference_set(a, b)
+})
+
+define_generic_procedure_handler(subtract, match_args(is_better_set, isAtom), (a: BetterSet<any>, b: any) => {
+    return remove_item(a, b)
+})
+
 
 
 export const is_equal = construct_simple_generic_procedure(
--- built_in_generics/generic_array.ts
+++ built_in_generics/generic_array.ts
@@ -1,5 +1,11 @@
-import { construct_simple_generic_procedure } from "../GenericProcedure"
+import { isArrowFunction } from "typescript";
+import { construct_simple_generic_procedure, define_generic_procedure_handler } from "../GenericProcedure"
+import { match_args } from "../Predicates";
+import { BetterSet, is_better_set } from "./generic_better_set";
 // only need to extend this four method to support map over custom array type
+
+// protocols
+
 export const get_element = construct_simple_generic_procedure("get_element", 2,
     <T>(array: T[], index: number): T => array[index]
 )
@@ -31,7 +37,8 @@ export const slice = <T>(array: T[], start: number, end: number = get_length(arr
     return result;
 }
 
-export const filter = <T>(array: T[], predicate: (item: T) => boolean): T[] => {
+
+export const _filter = <T>(array: T[], predicate: (item: T) => boolean): T[] => {
     let result: T[] = [];
     for (let i = 0; i < get_length(array); i++) {
         const item = get_element(array, i);
@@ -42,7 +49,8 @@ export const filter = <T>(array: T[], predicate: (item: T) => boolean): T[] => {
     return result;
 }
 
-export const map = <T, U>(array: T[], mapper: (item: T) => U): U[] => {
+
+export const _map = <T, U>(array: T[], mapper: (item: T) => U): U[] => {
     let result: U[] = [];
     for (let i = 0; i < get_length(array); i++) {
         result = push(result, mapper(get_element(array, i)));
@@ -50,13 +58,40 @@ export const map = <T, U>(array: T[], mapper: (item: T) => U): U[] => {
     return result;
 }
 
-export const reduce = <T, U>(array: T[], reducer: (acc: U, item: T) => U, initial: U): U => {
+export const _reduce = <T, U>(array: T[], reducer: (acc: U, item: T) => U, initial: U): U => {
     let acc = initial;
     for (let i = 0; i < get_length(array); i++) {
         acc = reducer(acc, get_element(array, i));
     }
     return acc;
 }
+
+   
+
+export const _for_each = <T>(array: T[], action: (item: T) => void): void => {
+    for (let i = 0; i < get_length(array); i++) {
+        action(get_element(array, i));
+    }
+} 
+
+ 
+
+export const _flat_map = <T, U>(array: T[], mapper: (item: T) => U[]): U[] => {
+    const flatten = (arr: any[], acc: U[]): U[] => {
+        if (get_length(arr) === 0) return acc;
+        const fst = first(arr)
+        const rst = rest(arr)   
+        if (isArray(fst)) {
+            return flatten([...fst, ...rst], acc);
+        } else {
+            return flatten(rst, push(acc, fst));
+        }
+    };
+
+    const mappedArray = array.map(mapper);
+    return flatten(mappedArray, []);
+}
+
 
 export const first = <T>(array: T[]): T => get_element(array, 0)
 
@@ -84,3 +119,59 @@ export const isEmpty = <T>(array: T[]): boolean =>
 
 
 export const isOriginalArray = isArray
+
+
+// operators
+
+export const filter = construct_simple_generic_procedure("filter", 2,
+    <T>(array: T[], predicate: (item: T) => boolean): T[] => {
+        return _filter(array, predicate)
+    }
+) 
+
+export const map = construct_simple_generic_procedure("map", 2,
+    <T, U>(array: T[], mapper: (item: T) => U): U[] => {
+        return _map(array, mapper)
+    }
+)
+
+export const for_each = construct_simple_generic_procedure("for_each", 2,
+    <T>(array: T[], action: (item: T) => void): void => {
+        return _for_each(array, action)
+    }
+)
+
+export const flat_map = construct_simple_generic_procedure("flat_map", 2,
+    <T, U>(array: T[], mapper: (item: T) => U[]): U[] => {
+        return _flat_map(array, mapper)
+    }
+)   
+
+export const reduce = construct_simple_generic_procedure("reduce", 3,
+    <T, U>(array: T[], reducer: (acc: U, item: T) => U, initial: U): U => {
+        return _reduce(array, reducer, initial)
+    }
+)
+
+
+/// 
+
+define_generic_procedure_handler(map, match_args(is_better_set, isArrowFunction), (set: BetterSet<any>, mapper) => {
+    return set.map(mapper)
+})
+
+define_generic_procedure_handler(filter, match_args(is_better_set, isArrowFunction), (set: BetterSet<any>, predicate) => {
+    return set.filter(predicate)
+}) 
+
+define_generic_procedure_handler(reduce, match_args(is_better_set, isArrowFunction, is_better_set), (set: BetterSet<any>, reducer, initial) => {
+    return set.reduce(reducer, initial)
+})
+
+define_generic_procedure_handler(for_each, match_args(is_better_set, isArrowFunction), (set: BetterSet<any>, action) => {
+    return set.for_each(action)
+})
+
+define_generic_procedure_handler(flat_map, match_args(is_better_set, isArrowFunction), (set: BetterSet<any>, mapper) => {
+    return set.flat_map(mapper)
+})
