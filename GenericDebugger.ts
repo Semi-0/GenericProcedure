3ec273b01db2ec1fdc9c995654bf85dada3100a5
PterRim <longpp17@163.com>
2024-09-08 23:57:28 +0200

    removed unused code

--- GenericDebugger.ts
+++ GenericDebugger.ts
@@ -34,15 +34,3 @@ export function summary_all_rules_with_args(procedure: (...args: any) => any, ar
     }
 }
 
-import { add } from "./built_in_generics/generic_arithmetic"
-import { is_array, is_atom, is_string } from "./built_in_generics/generic_predicates"
-
-define_generic_procedure_handler(add, match_args(is_string, is_string), (a: number, b: number) => a + b)
-
-define_generic_procedure_handler(add, match_args(is_atom, is_array), (a: number, b: number) => a + b)
-
-summary_all_rules(add).
-forEach(a => {
-    console.log(a)
-})
-
